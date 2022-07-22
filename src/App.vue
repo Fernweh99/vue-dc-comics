511b7e0b7d1f2e53e65648331b684ddde9307d40
Fernweh99 <daniele.dimento@gmail.com>
2022-07-22 15:35:14 +0200

    finish and add HeaderComponent

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,55 @@
 <template>
   <div>
-
+    <HeaderComponent/>
   </div>
 </template>
 
 <script>
+import HeaderComponent from "./components/HeaderComponent.vue"
 
 export default {
   name: 'DcSite',
   components: {
+    HeaderComponent,
   }
 }
 </script>
 
 <style lang="scss">
+  // Generics
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
+  ul {
+    list-style-type: none;
+  }
+
+  ul li {
+    display: inline-block;
+  }
+
+  a {
+    display: inline-block;
+    text-decoration: none;
+  }
+
+  body {
+    font-family: "Segoe UI", "Open Sans", Helvetica, sans-serif;;
+    background-color: #0E0E0E;
+  }
+  img {
+    height: auto;
+    max-width: 100%;
+  }
+  // Utils
+  .container {
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+  .h-100 {
+    height: 100%;
+  }
 </style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,111 @@
+<template>
+  <header>
+    <nav class="container">
+      <div class="col-img h-100">
+        <img src="../assets/img/dc-logo.png" alt="Dc Logo">
+      </div>
+      <div class="col-link h-100">
+        <ul class="h-100">
+          <li class="h-100" v-for="link in links" :key="link.text">
+            <a :href="link.url" :class="{'active' :link.current}">{{ link.text }}</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+  data() {
+    return {
+      links: [
+        {
+          text: 'Characters',
+          url: '#',
+          current: false,
+        },
+        {
+          text: 'Comics',
+          url: '#',
+          current: true,
+        },
+        {
+          text: 'Movies',
+          url: '#',
+          current: false,
+        },
+        {
+          text: 'TV',
+          url: '#',
+          current: false,
+        },
+        {
+          text: 'Games',
+          url: '#',
+          current: false,
+        },
+        {
+          text: 'Collectibles',
+          url: '#',
+          current: false,
+        },
+        {
+          text: 'Videos',
+          url: '#',
+          current: false,
+        },
+        {
+          text: 'Fans',
+          url: '#',
+          current: false,
+        },
+        {
+          text: 'News',
+          url: '#',
+          current: false,
+        },
+        {
+          text: 'Shop',
+          url: '#',
+          current: false,
+        },
+      ],
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  header {
+    background-color: #fff;
+    margin-top: 15px
+  }
+  nav {
+    display: flex;
+    width: 100%;
+    height: 90px;
+    justify-content: space-between;
+  }
+  img {
+    height: 100%;
+    padding: 10px;
+  }
+  .col-link a{
+   color: #707070;
+   text-transform: uppercase;
+   margin-left:25px;
+   font-size: 12px;
+   height:100%;
+   line-height: 90px;
+   font-weight: bold;
+   border-bottom: 2px solid transparent;
+  }
+  .col-link a.active,
+  .col-link a:hover {
+    color: #0082F9;
+    border-bottom: 3px solid #0082F9;
+  }
+  
+</style>
